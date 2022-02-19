3e6c0decd2e8a67f28fd212a1cf2dca3ff4d0e32
Morph21 <krzysztof.gr@hotmail.com>
2022-02-19 01:32:16 +0100

    Adding opencv search for ral runes
    Will add more in future

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,7 @@ namespace D2RMuler
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool DragWindowPossible = false;
         List<CharacterStash> CharacterStashes = new List<CharacterStash>();
         Controller ctrl;
 
@@ -28,6 +29,9 @@ namespace D2RMuler
             ctrl = new Controller();
 
             ctrl.SetupKeyboardHooks(new KeyBindAction(this));
+
+            //Dont hide taskbar on maximize
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
 
         private void ScreenShot_Click(object sender, RoutedEventArgs e)
@@ -159,18 +163,58 @@ namespace D2RMuler
             bitmapImage.StreamSource.Dispose();
             return bitmapImage;
         }
+        
+        private BitmapImage getImage(byte[] src)
+        {
+            //create new stream and create bitmap frame
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = new MemoryStream(src);
+            //bitmapImage.DecodePixelWidth = (int)_decodePixelWidth;
+            //bitmapImage.DecodePixelHeight = (int)_decodePixelHeight;
+            //load the image now so we can immediately dispose of the stream
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+
+            //clean up the stream to avoid file access exceptions when attempting to delete images
+            bitmapImage.StreamSource.Dispose();
+            return bitmapImage;
+        }
 
         private void MenuBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
+                DragWindowPossible = true;
+            }
+        }
+
+        private void MenuBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && DragWindowPossible)
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    Point mouseP = PointToScreen(Mouse.GetPosition(this));
+                    Top = mouseP.Y - MenuBar.ActualHeight / 2;
+                    Left = mouseP.X - Width / 2;
+                    WindowState = WindowState.Normal;
+                    //Top = mouseP.Y - MenuBar.ActualHeight/2;
+                    //Left = mouseP.X - Width/2;
+                }
                 DragMove();
             }
+        }
+
+        private void MenuBar_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            DragWindowPossible = false;
         }
 
         private void CloseWindowBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Close();
+            e.Handled = true;
         }
 
         private void WindowMaxize_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -183,11 +227,13 @@ namespace D2RMuler
             {
                 this.WindowState = WindowState.Maximized;
             }
+            e.Handled = true;
         }
 
         private void WindowMinimize_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
+            e.Handled = true;
         }
 
         public void Window_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -270,6 +316,134 @@ namespace D2RMuler
             KeyBindsWindow keyBindsWindow = new KeyBindsWindow();
             keyBindsWindow.Owner = this;
             keyBindsWindow.ShowDialog();
+        }
+
+        private void SearchForRalButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            CharacterStash item = CharactersListView.SelectedItem as CharacterStash;
+
+
+            Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> source = new Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte>(item.ImageSrc); // Bigger image
+
+            Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> template = new Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte>("ral_rune.png"); // smaller image
+            Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> imageToShow = source.Copy();
+
+
+            const double minTreshold = 0.9;
+            double actualTreshold = 1;
+            
+
+            while (actualTreshold > minTreshold)
+            {
+                Emgu.CV.Image<Emgu.CV.Structure.Gray, float> result = imageToShow.MatchTemplate(template, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed);
+                double[] minValues, maxValues;
+                System.Drawing.Point[] minLocations, maxLocations;
+                result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+                actualTreshold = maxValues[0];
+                if (maxValues[0] > minTreshold)
+                {
+                    // This is a match. Do something with it, for example draw a rectangle around it.
+                    System.Drawing.Rectangle match = new System.Drawing.Rectangle(maxLocations[0], template.Size);
+                    imageToShow.Draw(match, new Emgu.CV.Structure.Bgr(System.Drawing.Color.Red), 3);
+                    
+                }
+            }
+
+            StashImage.Source = getImage(imageToShow.ToJpegData());
+        }
+
+        private void test2(object sender, MouseButtonEventArgs e)
+        {
+            CharacterStash item = CharactersListView.SelectedItem as CharacterStash;
+            string imageSrc = item.ImageSrc;
+            File.Exists("ral_rune.png");
+            
+            Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> source = new Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte>(item.ImageSrc); // Bigger image
+           
+            Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> template = new Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte>("ral_rune.png"); // smaller image
+            Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> imageToShow = source.Copy();
+
+            using (Emgu.CV.Image<Emgu.CV.Structure.Gray, float> result = source.MatchTemplate(template, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed))
+            {
+                var listOfMaxs = new List<System.Drawing.Point>();
+                var resultWithPadding = new Emgu.CV.Image<Emgu.CV.Structure.Gray, float>(source.Size);
+                var heightOfPadding = (source.Height - result.Height) / 2;
+                var widthOfPadding = (source.Width - result.Width) / 2;
+                resultWithPadding.ROI = new System.Drawing.Rectangle() { X = heightOfPadding, Y = widthOfPadding, Width = result.Width, Height = result.Height };
+                result.CopyTo(resultWithPadding);
+                resultWithPadding.ROI = System.Drawing.Rectangle.Empty;
+
+                for (int i = 0; i < resultWithPadding.Width; i++)
+                {
+                    for (int j = 0; j < resultWithPadding.Height; j++)
+                    {
+                        var centerOfRoi = new System.Drawing.Point() { X = i + template.Width / 2, Y = j + template.Height / 2 };
+                        var roi = new System.Drawing.Rectangle() { X = i, Y = j, Width = template.Width, Height = template.Height };
+                        resultWithPadding.ROI = roi;
+                        double[] minValues, maxValues;
+                        System.Drawing.Point[] minLocations, maxLocations;
+                        resultWithPadding.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+                        resultWithPadding.ROI = System.Drawing.Rectangle.Empty;
+                        var maxLocation = maxLocations[0]; // First();
+                        if (maxLocation.X == roi.Width / 2 && maxLocation.Y == roi.Height / 2)
+                        {
+                            if (maxValues[0] > 0.9)
+                            {
+                                var point = new System.Drawing.Point() { X = i, Y= j };
+                                listOfMaxs.Add(point);
+
+                                System.Drawing.Rectangle match = new System.Drawing.Rectangle(point, template.Size);
+                                
+                                imageToShow.Draw(match, new Emgu.CV.Structure.Bgr(System.Drawing.Color.Red), 10);
+                            }
+                           
+                        }
+
+                    }
+                }
+
+            }
+
+            // Show imageToShow in an ImageBox (here assumed to be called imageBox1)
+
+            StashImage.Source = getImage(imageToShow.ToJpegData());
+            //imageBox1.Image = imageToShow;
+
+        }
+
+        private void test()
+        {
+            CharacterStash item = CharactersListView.SelectedItem as CharacterStash;
+            string imageSrc = item.ImageSrc;
+            File.Exists("ral_rune.png");
+
+            Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> source = new Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte>(item.ImageSrc); // Bigger image
+
+            Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> template = new Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte>("ral_rune.png"); // smaller image
+            Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> imageToShow = source.Copy();
+
+            using (Emgu.CV.Image<Emgu.CV.Structure.Gray, float> result = source.MatchTemplate(template, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed))
+            {
+                double[] minValues, maxValues;
+                System.Drawing.Point[] minLocations, maxLocations;
+
+
+
+                result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+
+                // You can try different values of the threshold. I guess somewhere between 0.75 and 0.95 would be good.
+                if (maxValues[0] > 0.9)
+                {
+                    // This is a match. Do something with it, for example draw a rectangle around it.
+                    System.Drawing.Rectangle match = new System.Drawing.Rectangle(maxLocations[0], template.Size);
+                    imageToShow.Draw(match, new Emgu.CV.Structure.Bgr(System.Drawing.Color.Red), 3);
+                }
+            }
+
+            // Show imageToShow in an ImageBox (here assumed to be called imageBox1)
+
+            StashImage.Source = getImage(imageToShow.ToJpegData());
+            //imageBox1.Image = imageToShow;
         }
 
     }
